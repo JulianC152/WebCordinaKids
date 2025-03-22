ed227174e54f1be1e6cf95097b69a6385cb1726d
EDUAR HERNAN TUQUERRES RUIZ <eduar.tuquerres.r@uniautonoma.edu.co>
2025-03-22 08:31:48 -0500

    se crea leyenda del footer

--- src/components/landing/FooterSection.vue
+++ src/components/landing/FooterSection.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="flex items-center space-x-4 p-4">
-    <!-- Contenedor de la imagen -->
-    <div>
-      <img src="/public/images/Autonoma.jpg" alt="" class="w-50 h-50 p-10" />
-    </div>
     <!-- Contenedor de los nombres alineados a la derecha -->
-    <div class="flex flex-col space-y-2 text-right">
-      <span>Nombre 1</span>
-      <span>Nombre 2</span>
-      <span>Nombre 3</span>
-      <span>Nombre 4</span>
-    </div>
+    <div class="flex flex-col space-y-2 text-center">
+    <p calss="text-xl font-bold">
+      (Coordi Kids), © 2025 es un producto del Tercer Laboratorio de Innovación Educativa "Herramientas didácticas para una generación digital", convocado por La Cátedra UNESCO Movimiento Educativo Abierto para América Latina del Tecnológico de Monterrey y la Especialización en Pedagogía de la Facultad de Ciencias Sociales y Humanidades de la Corporación Universitaria Autónoma del Cauca, con apoyo del Centro Universitario de la Universidad Autónoma del Estado de México - Texcoco, la Universidad Francisco de Paula Santander y la Universidad Filadelfia de México. En el marco del proyecto educativo “Herramientas didácticas para una generación digital ” by Promotoras: Katherin Diaz Ordoñez, Julian Andres Charfuelan Sanchez, Eduar Hernan Tuquerrez Ruiz, Emmanuel Villa Hoyos, Edwin Hernan Daza Bermeo y Luis Eduardo López Chilito, Mentor/a: Dra Ana Mendieta L: Magister, Luz Marina Chalapud Narváez is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+    </p>
+  </div>
+  <!-- Contenedor de la imagen -->
+  <div>
+      <img src="/public/images/Autonoma.jpg" alt="" class="w-50 h-50 p-10" />
+  </div>
   </div>
 </template>
